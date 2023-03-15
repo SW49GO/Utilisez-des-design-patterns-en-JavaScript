2bd42804a5d3ec54c6c3b2046ef03acdf91fe184
SW49GO <gohard.so49@gmail.com>
2023-03-15 07:41:14 +0100

    Adjust path

--- js/App.js
+++ js/App.js
@@ -1,18 +1,25 @@
 class App {
-    constructor() {
-        this.$moviesWrapper = document.querySelector('.movies-wrapper')
-        this.moviesApi = new MovieApi('/data/movie-data.json')
-    }
+  constructor() {
+    // stockage de la référence ayant la class .movies-wrapper dans $moviesWrapper
+    this.$moviesWrapper = document.querySelector('.movies-wrapper');
+    // initialisation de moviesApi à une instance de MovieApi en passane le chemin d'accès au fichier JSON
+    this.moviesApi = new MovieApi('data/movie-data.json');
+  }
 
-    async main() {
-        const movies = await this.moviesApi.getMovies()
+  async main() {
+    // attend que la methode getMovies() de la propriété moviesApi retourne la liste des films (avec promesse)
+    const movies = await this.moviesApi.getMovies();
 
-        movies.forEach(movie => {
-            const Template = new MovieCard(movie)
-            this.$moviesWrapper.appendChild(Template.createMovieCard())        
-        })    
-    }
+    // boucle sur la liste, et crée une instance de MovieCard avec les paramètres correspondant
+    // ce qui va crée la carte en HTML
+    movies.forEach((movie) => {
+      const Template = new MovieCard(movie);
+      // la méthode createMovieCard est appelé sur chaque instance de MovieCard(movie), qui retourne un élémentHTML
+      // qui est ajouter à l'élément this.$moviesWrapper avec appendChild
+      this.$moviesWrapper.appendChild(Template.createMovieCard());
+    });
+  }
 }
 
-const app = new App()
-app.main()
+const app = new App();
+app.main();
